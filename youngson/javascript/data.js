8c0779c5c8257d56b4dac3347d0f2bab2f11485e
YoungSun <shonys1214@naver.com>
2025-09-30 18:15:19 +0900

    0930 완

---
+++ youngson/javascript/data.js
@@ -0,0 +1,427 @@
+export const shopData1 = [
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/21/12/51//134/134_magnify_061.jpg",
+    item_name:
+      "[3종세트] 다이브인 저분자 히알루론산 세럼 50ml + 토너 300ml + 크림80ml+(쇼핑백 증정)",
+    item_sale: 42,
+    item_price: 64000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/09/38/304/304_main_062.jpg",
+    item_name: "[라인콜라보] 다이브인 저분자 히알루론산 마스크 27ml 10매",
+    item_sale: 50,
+    item_price: 30000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/08/32/291/176_magnify_054.jpg",
+    item_name:
+      "[회원전용특가] [2개묶음 기획세트] 다이브인 워터리 모이스처 선크림 60ml 1+1",
+    item_sale: 40,
+    item_price: 25000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/23/09/39/185/185_magnify_098.jpg",
+    item_name: "셀메이징 저분자 콜라겐 탄력 겔 마스크 1매",
+    item_sale: 25,
+    item_price: 7500,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/04/14/245/245_magnify_054.jpg",
+    item_name: "[2개묶음] 밸런스풀 시카 마스크 25ml 20매 (10매 x2개)",
+    item_sale: 51,
+    item_price: 60000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/31/272/272_add3_050.jpg",
+    item_name: "[2개묶음] 다이브인 저분자 히알루론산 클렌징 오일 200ml",
+    item_sale: 18,
+    item_price: 44000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/04/17/253/253_main_034.jpg",
+    item_name: "다이브인 저분자 히알루론산 클렌징 오일 200ml",
+    item_sale: 18,
+    item_price: 22000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/08/31/283/283_magnify_091.jpg",
+    item_name: "[2개묶음] 밸런스풀 시카 진정 크림 80ml",
+    item_sale: 42,
+    item_price: 48000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/12/51/162/162_magnify_019.jpg",
+    item_name: "밸런스풀 시카 진정 크림 80ml",
+    item_sale: 30,
+    item_price: 24000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/24/12/50/232/232_main_067.jpg",
+    item_name: "[2개묶음] 다이브인 저분자 히알루론산 스킨부스터 200ml+200ml",
+    item_sale: 19,
+    item_price: 36000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/19/09/04/68/68_main_024.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 크림 80ml+스킨부스터 200ml",
+    item_sale: 20,
+    item_price: 39000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/24/10/41/230/230_magnify_0100.jpg",
+    item_name: "밸런스풀 시카 각질 토너 250ml",
+    item_sale: 0,
+    item_price: 24000,
+  },
+];
+
+export const shopData2 = [
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/23/09/39/186/186_main_076.jpg",
+    item_name: "셀메이징 저분자 콜라겐 탄력 크림 60ml",
+    item_sale: 33,
+    item_price: 30000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/23/02/07/169/169_main_061.jpg",
+    item_name: "밸런스풀 시카 마스크 25ml 10매",
+    item_sale: 50,
+    item_price: 30000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/08/31/282/282_main_047.jpg",
+    item_name: "[2개묶음] 밸런스풀 시카 토너 패드 60매",
+    item_sale: 30,
+    item_price: 46000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/05/19/257/257_main_027.jpg",
+    item_name: "셀메이징 비타 C 브라이트닝 흔적 토닝 패드 70매",
+    item_sale: 33,
+    item_price: 23000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/10/43/156/156_main_080.jpg",
+    item_name: "밸런스풀 시카 토너 패드 60매",
+    item_sale: 33,
+    item_price: 23000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/12/52/163/163_main_083.jpg",
+    item_name:
+      "[3종세트] 밸런스풀 세럼50ml + 크림80ml + 토너패드60매+(쇼핑백 증정)",
+    item_sale: 44,
+    item_price: 70000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/10/43/158/158_main_097.jpg",
+    item_name: "밸런스풀 시카 클렌징 젤 200ml",
+    item_sale: 17,
+    item_price: 20000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/08/31//154/154_main_039.jpg",
+    item_name: "[2개묶음] 다이브인 저분자 히알루론산 클렌징 폼 150ml",
+    item_sale: 33,
+    item_price: 30000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/09/37/298/298_main_018.jpg",
+    item_name:
+      "[가나디 콜라보] [총 100ml] 다이브인 저분자 히알루론산 세럼 50ml (+리필50ml+인형키링)",
+    item_sale: 0,
+    item_price: 36000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/23/08/32/182/182_main_028.jpg",
+    item_name: "[2종세트] 토리든 다이브인 저분자 히알루론산 세럼 50ml+40ml",
+    item_sale: 28,
+    item_price: 42000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/07/24/88/88_main_02.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml +다이브인 히알루론산 저분자 토너 300ml",
+    item_sale: 25,
+    item_price: 43000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/07/28//152/152_main_0100.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 클렌징 워터 센서티브 400ml + 다이브인 마일드 무기자차 선크림 60ml",
+    item_sale: 22,
+    item_price: 43000,
+  },
+];
+
+export const shopData3 = [
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/21/12/51//133/133_main_038.jpg",
+    item_name: "[3종세트] 다이브인 세럼50ml + 토너300ml + 수딩크림100ml",
+    item_sale: 40,
+    item_price: 67000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/08/20/93/93_main_059.jpg",
+    item_name:
+      "[2종세트] 다이브인 히알루론산 저분자 세럼50ml+다이브인 히알루론산 멀티패드80매",
+    item_sale: 33,
+    item_price: 45000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/21/12/51//135/135_main_053.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml + 수딩크림 100ml",
+    item_sale: 35,
+    item_price: 46000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/08/31/280/280_main_071.jpg",
+    item_name:
+      "[2개묶음] 다이브인 저분자 히알루론산 마스크 27ml 20매 (10매x2개)",
+    item_sale: 51,
+    item_price: 60000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/12/50/114/114_main_057.jpg",
+    item_name: "다이브인 저분자 히알루론산 마스크 27ml 10매",
+    item_sale: 50,
+    item_price: 30000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/04/17//143/143_main_048.jpg",
+    item_name: "[2개묶음] 다이브인 무기자차 마일드 선크림 60ml",
+    item_sale: 35,
+    item_price: 50000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/19/09/04/69/69_main_046.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml + 스킨부스터 200ml",
+    item_sale: 23,
+    item_price: 40000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/19/09/04/71/71_main_062.jpg",
+    item_name: "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml + 크림 80ml",
+    item_sale: 35,
+    item_price: 43000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/01/08/77/77_main_031.jpg",
+    item_name: "[2개묶음] 다이브인 저분자 히알루론산 크림 80ml",
+    item_sale: 30,
+    item_price: 42000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/10/43/100/100_main_061.jpg",
+    item_name: "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml + 마스크 10매",
+    item_sale: 39,
+    item_price: 52000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/11/48/110/110_main_013.jpg",
+    item_name: "[2개묶음] 다이브인 저분자 히알루론산 토너 300ml",
+    item_sale: 28,
+    item_price: 42000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/03/13//142/142_main_02.jpg",
+    item_name:
+      "[2종세트] 다이브인 저분자 히알루론산 세럼 50ml + 마일드 무기자차 선크림 60ml",
+    item_sale: 27,
+    item_price: 47000,
+  },
+];
+export const shopData4 = [
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/09/37/299/299_main_081.jpg",
+    item_name:
+      "[가나디 콜라보] [1+1 기획세트] 다이브인 저분자 히알루론산 수딩 크림 100ml+(파우치키링)",
+    item_sale: 0,
+    item_price: 42000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/21/08/31/132/132_main_089.jpg",
+    item_name: "다이브인 저분자 히알루론산 수딩 크림 100ml",
+    item_sale: 25,
+    item_price: 24000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/18/12/27/58/58_main_044.jpg",
+    item_name: "다이브인 저분자 히알루론산 세럼 50ml",
+    item_sale: 23,
+    item_price: 22000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/20/07/28/90/90_main_032.jpg",
+    item_name: "다이브인 저분자 히알루론산 토너 300ml",
+    item_sale: 23,
+    item_price: 21000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/18/12/24/57/57_main_07.jpg",
+    item_name: "다이브인 저분자 히알루론산 크림 80ml",
+    item_sale: 21,
+    item_price: 21000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/29/264/264_main_057.jpg",
+    item_name:
+      "[NEW] [2개묶음] 다이브인 저분자 히알루론산 클렌징 워터 센서티브 400ml",
+    item_sale: 35,
+    item_price: 40000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/29/263/263_main_012.jpg",
+    item_name: "[NEW] 다이브인 저분자 히알루론산 클렌징 워터 센서티브 400ml",
+    item_sale: 20,
+    item_price: 20000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/22/06/25//145/145_main_018.jpg",
+    item_name: "[회원전용특가] 다이브인 저분자 히알루론산 클렌징 워터 400ml",
+    item_sale: 55,
+    item_price: 20000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/31/278/278_main_097.jpg",
+    item_name:
+      "[2개세트] 다이브인 워터리 핏 선세럼 50ml+다이브인 모이스처 선크림 60ml",
+    item_sale: 21,
+    item_price: 5000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/31/269/269_main_090.jpg",
+    item_name: "[2개묶음] 다이브인 워터리 핏 선세럼 50ml",
+    item_sale: 26,
+    item_price: 50000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/07/31/268/268_main_069.jpg",
+    item_name: "[2개묶음] 다이브인 퍼플 톤업 선크림 60ml",
+    item_sale: 38,
+    item_price: 50000,
+  },
+  {
+    item_src:
+      "https://torridtr9977.cdn-nhncommerce.com/data/goods/25/04/15/251/251_main_025.jpg",
+    item_name: "다이브인 퍼플 톤업 선크림 60ml",
+    item_sale: 28,
+    item_price: 25000,
+  },
+];
+export const shopData5 = [
+  { item_src: "", item_name: "", item_sale: 12, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 35, item_price: 50000 },
+  { item_src: "", item_name: "", item_sale: 28, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 28, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 29, item_price: 14000 },
+  { item_src: "", item_name: "", item_sale: 29, item_price: 14000 },
+  { item_src: "", item_name: "", item_sale: 42, item_price: 50000 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 7000 },
+  { item_src: "", item_name: "", item_sale: 35, item_price: 50000 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 1500 },
+  { item_src: "", item_name: "", item_sale: 20, item_price: 47000 },
+  { item_src: "", item_name: "", item_sale: 26, item_price: 44000 },
+];
+export const shopData6 = [
+  { item_src: "", item_name: "", item_sale: 30, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 9, item_price: 22000 },
+  { item_src: "", item_name: "", item_sale: 30, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 17, item_price: 18000 },
+  { item_src: "", item_name: "", item_sale: 26, item_price: 23000 },
+  { item_src: "", item_name: "", item_sale: 30, item_price: 23000 },
+  { item_src: "", item_name: "", item_sale: 25, item_price: 23000 },
+  { item_src: "", item_name: "", item_sale: 30, item_price: 46000 },
+  { item_src: "", item_name: "", item_sale: 28, item_price: 23000 },
+  //   itemsale, price 둘다 0일경우 신규회원 전용상품
+  { item_src: "", item_name: "", item_sale: 0, item_price: 0 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 0 },
+  { item_src: "", item_name: "", item_sale: 47, item_price: 19000 },
+];
+export const shopData7 = [
+  { item_src: "", item_name: "", item_sale: 30, item_price: 30000 },
+  { item_src: "", item_name: "", item_sale: 12, item_price: 15000 },
+  { item_src: "", item_name: "", item_sale: 12, item_price: 25000 },
+  { item_src: "", item_name: "", item_sale: 14, item_price: 14000 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 7000 },
+  { item_src: "", item_name: "", item_sale: 20, item_price: 26000 },
+  { item_src: "", item_name: "", item_sale: 30, item_price: 3000 },
+  { item_src: "", item_name: "", item_sale: 33, item_price: 40000 },
+  { item_src: "", item_name: "", item_sale: 12, item_price: 15000 },
+  { item_src: "", item_name: "", item_sale: 20, item_price: 28000 },
+  { item_src: "", item_name: "", item_sale: 46, item_price: 60000 },
+  { item_src: "", item_name: "", item_sale: 27, item_price: 52000 },
+];
+export const shopData8 = [
+  { item_src: "", item_name: "", item_sale: 23, item_price: 26000 },
+  { item_src: "", item_name: "", item_sale: 42, item_price: 52000 },
+  { item_src: "", item_name: "", item_sale: 40, item_price: 67000 },
+  { item_src: "", item_name: "", item_sale: 51, item_price: 60000 },
+  { item_src: "", item_name: "", item_sale: 33, item_price: 43000 },
+  { item_src: "", item_name: "", item_sale: 29, item_price: 14000 },
+  { item_src: "", item_name: "", item_sale: 30, item_price: 51500 },
+  { item_src: "", item_name: "", item_sale: 32, item_price: 45000 },
+  { item_src: "", item_name: "", item_sale: 55, item_price: 60000 },
+  { item_src: "", item_name: "", item_sale: 27, item_price: 34000 },
+  { item_src: "", item_name: "", item_sale: 35, item_price: 24000 },
+  { item_src: "", item_name: "", item_sale: 31, item_price: 22000 },
+];
+export const shopData9 = [
+  { item_src: "", item_name: "", item_sale: 10, item_price: 21000 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 7000 },
+  { item_src: "", item_name: "", item_sale: 46, item_price: 56000 },
+  { item_src: "", item_name: "", item_sale: 38, item_price: 55000 },
+  { item_src: "", item_name: "", item_sale: 0, item_price: 3000 },
+  { item_src: "", item_name: "", item_sale: 50, item_price: 30000 },
+  { item_src: "", item_name: "", item_sale: 38, item_price: 26000 },
+];
---
+++ youngson/javascript/shop1.js
@@ -0,0 +1,56 @@
+import { shopData1 } from "../javascript/data.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const shopItemWrap = document.getElementById("shop_item_wrap");
+
+  shopData1.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "shop_item_card";
+
+    const hasSale = item.item_sale > 0;
+    const priceAfter = hasSale
+      ? Math.floor(
+          item.item_price * (1 - item.item_sale / 100)
+        ).toLocaleString() + "원"
+      : item.item_price.toLocaleString() + "원";
+
+    card.innerHTML = `
+      <div class="shop_item_img_wrap">
+        <img src="${item.item_src}"/>
+        <a class="shop_heart_btn">
+          <img src="../ys_img/icon_heartbtn.svg" alt="" />
+        </a>
+      </div>
+      <div class="shop_item_info_wrap">
+        <div class="shop_item_name">${item.item_name}</div>
+        <div class="shop_item_price_wrap">
+          ${
+            hasSale
+              ? `<div class="shop_item_price_sale">${item.item_sale}%</div>`
+              : ""
+          }
+          <div class="shop_item_price_after">${priceAfter}</div>
+          ${
+            hasSale
+              ? `<div class="shop_item_price_before">${item.item_price.toLocaleString()}원</div>`
+              : ""
+          }
+        </div>
+      </div>
+      <div class="shop_item_cart_wrap">
+        <span>장바구니 담기</span>
+        <img src="../ys_img/icon_cart.svg" alt="장바구니" />
+      </div>
+    `;
+    // 마우스 오버 이미지 블러
+    const img = card.querySelector(".shop_item_img_wrap img");
+    img.addEventListener("mouseover", () => {
+      img.style.transition = "filter 0.1s ease";
+      img.style.filter = "blur(1px)";
+      setTimeout(() => {
+        img.style.filter = "blur(0)";
+      }, 200);
+    });
+    shopItemWrap.appendChild(card);
+  });
+});
